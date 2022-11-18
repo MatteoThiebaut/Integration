90e6a43029815f113367d1d74c000b9333a29a5d
MatteoThiebaut <Lomips@hotmail.fr>
2022-11-18 15:13:29 +0100

    ts

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "CI/CD rocks!" );
+    	String unusedVariable;
+    	// Comment
+        System.out.println("Hello Big World!");
     }
 }
